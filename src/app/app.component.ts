f3c38f76ac7f102daf1aa75bc6e5147dbec104da
Aleksandar <aleksandarichev@github.com>
2023-05-25 12:40:56 +0200

    profile card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,90 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  transition,
+  animate,
+} from '@angular/animations';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('flipState', [
+      state(
+        'active',
+        style({
+          transform: 'rotateY(179deg)',
+        })
+      ),
+      state(
+        'inactive',
+        style({
+          transform: 'rotateY(0)',
+        })
+      ),
+      transition('active => inactive', animate('500ms ease-out')),
+      transition('inactive => active', animate('500ms ease-in')),
+    ]),
+  ],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'profile-card';
+  firstName = 'John';
+  lastName = 'Doe';
+  officeLocation = 'One Inside';
+
+  editProfile: FormGroup;
+  isFormSubmitted = false;
+
+  flip: string = 'inactive';
+
+  constructor() {}
+
+  ngOnInit() {
+    this.initForm();
+  }
+
+  toggleFlip() {
+    this.flip = this.flip == 'inactive' ? 'active' : 'inactive';
+  }
+
+  initForm() {
+    this.editProfile = new FormGroup({
+      firstName: new FormControl<string>('', {
+        nonNullable: true,
+      }),
+      lastName: new FormControl<string>('', {
+        nonNullable: true,
+      }),
+      officeLocation: new FormControl<string>('', {
+        nonNullable: true,
+      }),
+    });
+  }
+
+  onFormSubmit() {
+    this.isFormSubmitted = true;
+    if (this.editProfile.invalid) return;
+
+    const { firstName, lastName, officeLocation } = this.editProfile.value;
+    if (!firstName && !lastName && !officeLocation) return;
+
+    localStorage.setItem('form-data', JSON.stringify(this.editProfile.value));
+
+    const formValues = localStorage.getItem('form-data');
+    if (!formValues) return;
+    const data = JSON.parse(formValues);
+
+    this.firstName = data.firstName;
+    this.lastName = data.lastName;
+    this.officeLocation = data.officeLocation;
+
+    this.editProfile.reset();
+
+    this.flip = this.flip == 'inactive' ? 'active' : 'inactive';
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
